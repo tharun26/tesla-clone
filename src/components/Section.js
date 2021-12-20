5e51e47c55d8df87231316d53e0b20d7024ad001
tharun gowrishankar <tharun26@gmail.com>
2021-12-20 16:40:54 +0530

    ADD:redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 function Section({
   title,
@@ -11,15 +12,20 @@ function Section({
 }) {
   return (
     <Wrap bgImage={backgroundImg} className="section">
-    <ItemText>
-        <h1> {title} </h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1> {title} </h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
+
         <DownArrrow src={`images/down-arrow.svg`} />
       </Buttons>
     </Wrap>
@@ -29,6 +35,7 @@ function Section({
 export default Section;
 
 const Wrap = styled.div`
+
   width: 100vw;
   height: 100vh;
   background-image: ${(props) => `url("/images/${props.bgImage}")`};
